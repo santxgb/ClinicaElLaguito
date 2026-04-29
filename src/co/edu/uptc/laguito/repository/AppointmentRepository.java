197713410f6ed328c9b76141bf7234bf303efdc1
santxgb <gabrielsantiagobordacurrea@gmail.com>
2026-04-29 16:29:05 -0500

    Terminé la clase de AppointmentRepository de tal manera que toma todas las citas y las ordena por hora. Pero también agregando la prioridad de la cita, de tal forma que en dado caso de que las citas sean a la misma hora se priorice una por encima de otra según su nivel de prioridad

--- src/co/edu/uptc/laguito/repository/AppointmentRepository.java
+++ src/co/edu/uptc/laguito/repository/AppointmentRepository.java
@@ -1,5 +1,46 @@
 package co.edu.uptc.laguito.repository;
 
+import co.edu.uptc.laguito.domain.MedicalAppointment;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+
 public class AppointmentRepository {
+		
+		private HashMap<Integer, MedicalAppointment> appointments;
+		
+		public AppointmentRepository() {
+			this.appointments = new HashMap<>();
+		}
+		
+		public boolean addAppointment(MedicalAppointment appointment) {
+			if(appointments.containsKey(appointment.getIdMedicalAppointment())) {
+				return false;
+		}
+			appointments.put(appointment.getIdMedicalAppointment(), appointment);
+			return true;
+		}
+		
+		public HashMap<Integer, MedicalAppointment> findAll(){
+			return appointments;
+		}
+		
+		public MedicalAppointment findById(Integer idMedicalAppointment) {
+			return appointments.get(idMedicalAppointment);
+		}
+		
+		public TreeSet<MedicalAppointment> findAllByTimeAndPriority(){
+			TreeSet<MedicalAppointment> orden = new TreeSet<>((a, b) ->{
+				int byTime = a.getTimeAppointment().compareTo(b.getTimeAppointment());
+				if(byTime == 0) {
+					return Integer.compare(b.getPatient().getPriority().getValue(),
+							a.getPatient().getPriority().getValue()
+							);
+				}
+				return byTime;
+			});
+			orden.addAll(appointments.values());
+			return orden;
+		}
 
 }
