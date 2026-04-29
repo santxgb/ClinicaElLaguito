c7b271f5c795f71e29b8c1f3385ce434d5ac8fa6
santxgb <gabrielsantiagobordacurrea@gmail.com>
2026-04-29 09:54:37 -0500

    Agregué los atributos de la clase de doctor junto con los llamados a los enums, su constructor, los getters y setters e implementé el metodo de ComparableTo. Por último puse el toString

--- src/co/edu/uptc/model/Doctor.java
+++ src/co/edu/uptc/model/Doctor.java
@@ -1,7 +1,9 @@
 package co.edu.uptc.model;
+import co.edu.uptc.model.enums.*;
+
 
 public class Doctor implements Comparable<Doctor>{
-	
+    
 	private IdentificationType identificationType;
     private int medicalId;
     private String firstName;
@@ -19,7 +21,71 @@ public class Doctor implements Comparable<Doctor>{
 		this.specialty = specialty;
 		this.yearsOfExperience = yearsOfExperience;
 	}
-    
-    
+	
+	public IdentificationType getIdentificationType() {
+		return identificationType;
+	}
+
+	public void setIdentificationType(IdentificationType identificationType) {
+		this.identificationType = identificationType;
+	}
+
+	public int getMedicalId() {
+		return medicalId;
+	}
+
+	public void setMedicalId(int medicalId) {
+		this.medicalId = medicalId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+
+	public int getYearsOfExperience() {
+		return yearsOfExperience;
+	}
+
+	public void setYearsOfExperience(int yearsOfExperience) {
+		this.yearsOfExperience = yearsOfExperience;
+	}
+
+	@Override
+	public int compareTo(Doctor o) {
+		int porExp = Integer.compare(this.yearsOfExperience, o.yearsOfExperience);
+		if(porExp == 0) {
+			String fullName = this.firstName + "" + this.lastName;
+			String fullNameO = o.firstName + "" + o.lastName;
+			return fullName.compareTo(fullNameO);
+		}
+		return porExp;
+	}
+
+	@Override
+	public String toString() {
+		return "Doctor [identificationType=" + identificationType + ", medicalId=" + medicalId + ", firstName="
+				+ firstName + ", lastName=" + lastName + ", specialty=" + specialty + ", yearsOfExperience="
+				+ yearsOfExperience + "]";
+	}
 	
 }
