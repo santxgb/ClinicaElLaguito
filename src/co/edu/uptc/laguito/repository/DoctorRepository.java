ecdfac37aa58b451b7c589cf092b42730f94b0ba
santxgb <gabrielsantiagobordacurrea@gmail.com>
2026-04-30 12:31:39 -0500

    Agregué la documentación de las dos clases (PatientRepository y DoctorRepository), tengo que revisar el index.html que se crea ya que solo se vé la documentación de la clase Doctor

--- src/co/edu/uptc/laguito/repository/DoctorRepository.java
+++ src/co/edu/uptc/laguito/repository/DoctorRepository.java
@@ -4,15 +4,32 @@ import co.edu.uptc.laguito.domain.Doctor;
 import java.util.HashMap;
 import java.util.TreeSet;
 
+/**
+ * <b>Descripción: </b> Clase repositorio que gestiona el almacenamiento
+ * y consulta de los médicos del sistema <br>
+ *
+ * @author Santiago
+ */
+
 public class DoctorRepository {
 	
+	/** Atributo que determina el mapa de médicos registrados en el sistema */
 	private HashMap<Integer, Doctor> doctors;
 
+	/**
+     * <b>Descripción: </b> Constructor de la clase
+     */
 	public DoctorRepository() {
 		super();
 		this.doctors = new HashMap<>();
 	}
 	
+	/**
+     * <b>Descripción: </b> Agrega un médico al sistema validando que no exista
+     * un registro con el mismo identificador <br>
+     * @param doctor Parámetro que determina el médico a registrar
+     * @return true si el médico fue registrado, false si el identificador ya existe
+     */
 	public boolean addDoctor(Doctor doctor) {
 		if(doctors.containsKey(doctor.getMedicalId())) {
 			return false;
@@ -21,14 +38,28 @@ public class DoctorRepository {
 		return true;
 	}
 	
+	/**
+     * <b>Descripción: </b> Retorna todos los médicos registrados en el sistema <br>
+     * @return HashMap con todos los médicos registrados
+     */
 	public HashMap<Integer, Doctor> findAll(){
 		return doctors;
 	}
 	
+	/**
+     * <b>Descripción: </b> Busca y retorna un médico por su número de identificación médica <br>
+     * @param medicalId Parámetro que determina el número de identificación médica
+     * @return El médico encontrado, o null si no existe
+     */
 	public Doctor findById(Integer medicalId) {
 		return doctors.get(medicalId);
 	}
 	
+	/**
+     * <b>Descripción: </b> Retorna todos los médicos ordenados por años de experiencia
+     * de forma ascendente y en caso de empate por nombre completo de forma ascendente <br>
+     * @return TreeSet con los médicos ordenados por experiencia y nombre
+     */
 	public TreeSet<Doctor> findAllByExperience() {
 		TreeSet<Doctor> orden = new TreeSet<>();
 		orden.addAll(doctors.values());
--- src/co/edu/uptc/laguito/repository/PatientRepository.java
+++ src/co/edu/uptc/laguito/repository/PatientRepository.java
@@ -4,16 +4,35 @@ import co.edu.uptc.laguito.domain.Patient;
 import java.util.HashMap;
 import java.util.HashSet;
 
+/**
+ * <b>Descripción: </b> Clase repositorio que gestiona el almacenamiento
+ * y consulta de los pacientes del sistema <br>
+ *
+ * @author Santiago
+ */
 public class PatientRepository {
-
+	
+	/** Atributo que determina el mapa de pacientes registrados en el sistema */
 	private HashMap<Integer, Patient> patients;
+	
+	/** Atributo que determina el conjunto de correos electrónicos ya registrados */
 	private HashSet<String> registeredEmails;
+	
+	/**
+     * <b>Descripción: </b> Constructor de la clase
+     */
 	public PatientRepository() {
 		super();
 		this.patients = new HashMap<>();
 		this.registeredEmails = new HashSet<>();
 	}
 	
+	/**
+     * <b>Descripción: </b> Agrega un paciente al sistema validando que no exista
+     * un registro con el mismo identificador ni con el mismo correo electrónico <br>
+     * @param patient Parámetro que determina el paciente a registrar
+     * @return true si el paciente fue registrado, false si el ID o el email ya existen
+     */
 	public boolean addPatient(Patient patient) {
 		if(patients.containsKey(patient.getIdPatient())) {
 			return false;
@@ -25,14 +44,30 @@ public class PatientRepository {
 		return true;
 	}
 	
+	/**
+     * <b>Descripción: </b> Retorna todos los pacientes registrados en el sistema <br>
+     * @return HashMap con todos los pacientes registrados
+     */
 	public HashMap<Integer, Patient> findAll(){
 		return patients;
 	}
 	
+	/**
+     * <b>Descripción: </b> Busca y retorna un paciente por su número de identificación <br>
+     * @param idPatient Parámetro que determina el número de identificación del paciente
+     * @return El paciente encontrado, o null si no existe
+     */
 	public Patient findById(Integer idPatient) {
 		return patients.get(idPatient);
 	}
 	
+	/**
+     * <b>Descripción: </b> Agrega un medicamento al historial de un paciente
+     * validando que el paciente exista y que el medicamento no esté ya registrado <br>
+     * @param idPatient Parámetro que determina el ID del paciente
+     * @param medication Parámetro que determina el medicamento a agregar
+     * @return true si el medicamento fue agregado, false si el paciente no existe o el medicamento ya estaba
+     */
 	public boolean addMedication(Integer idPatient, String medication) {
 		Patient patient = findById(idPatient);
 		if(patient == null) {
