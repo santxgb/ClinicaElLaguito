d1025a08da705595a5201628765b9fedcc954959
santxgb <gabrielsantiagobordacurrea@gmail.com>
2026-04-30 11:54:44 -0500

    Agregué la clase de Doctor repository, que hace algo muy parecido a lo de patient en el tema de los id's. Pero en este caso también da el orden de los doctores con el TreeSet por su experiencia

--- src/co/edu/uptc/laguito/repository/DoctorRepository.java
+++ src/co/edu/uptc/laguito/repository/DoctorRepository.java
@@ -1,5 +1,38 @@
 package co.edu.uptc.laguito.repository;
 
-public class DoctorRepository {
+import co.edu.uptc.laguito.domain.Doctor;
+import java.util.HashMap;
+import java.util.TreeSet;
 
+public class DoctorRepository {
+	
+	private HashMap<Integer, Doctor> doctors;
+
+	public DoctorRepository() {
+		super();
+		this.doctors = new HashMap<>();
+	}
+	
+	public boolean addDoctor(Doctor doctor) {
+		if(doctors.containsKey(doctor.getMedicalId())) {
+			return false;
+		}
+		doctors.put(doctor.getMedicalId(), doctor);
+		return true;
+	}
+	
+	public HashMap<Integer, Doctor> findAll(){
+		return doctors;
+	}
+	
+	public Doctor findById(Integer medicalId) {
+		return doctors.get(medicalId);
+	}
+	
+	public TreeSet<Doctor> findAllByExperience() {
+		TreeSet<Doctor> orden = new TreeSet<>();
+		orden.addAll(doctors.values());
+		return orden;
+	}
+	
 }
