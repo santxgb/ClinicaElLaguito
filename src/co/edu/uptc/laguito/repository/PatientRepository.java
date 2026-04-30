de81187ff3dbf6df2c7412591c91d57cb55cc32f
santxgb <gabrielsantiagobordacurrea@gmail.com>
2026-04-30 11:24:01 -0500

    Agregué el código de la clase de Patient Repository. Guarda los pacientes, evita si tienen id o email repetido, busca los pacientes y agrega los medicamentos al historial de los pacientes

--- src/co/edu/uptc/laguito/repository/PatientRepository.java
+++ src/co/edu/uptc/laguito/repository/PatientRepository.java
@@ -1,5 +1,44 @@
 package co.edu.uptc.laguito.repository;
 
+import co.edu.uptc.laguito.domain.Patient;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class PatientRepository {
+
+	private HashMap<Integer, Patient> patients;
+	private HashSet<String> registeredEmails;
+	public PatientRepository(HashMap<Integer, Patient> patients, HashSet<String> registeredEmails) {
+		super();
+		this.patients = new HashMap<>();
+		this.registeredEmails = new HashSet<>();
+	}
+	
+	public boolean addPatient(Patient patient) {
+		if(patients.containsKey(patient.getIdPatient())) {
+			return false;
+		}
+		if(!registeredEmails.add(patient.getEmail())) {
+			return false;
+		}
+		patients.put(patient.getIdPatient(), patient);
+		return true;
+	}
+	
+	public HashMap<Integer, Patient> findAll(){
+		return patients;
+	}
+	
+	public Patient findById(Integer idPatient) {
+		return patients.get(idPatient);
+	}
+	
+	public boolean addMedication(Integer idPatient, String medication) {
+		Patient patient = findById(idPatient);
+		if(patient == null) {
+			return false;
+		}
+		return patient.getMedicationHistory().add(medication);
+	}
 
 }
